b1db0a3e7a1379103a65b4e5e8eecad3a6bb8622
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-05-04 12:33:06 +0200

    Added basic floor button

---
+++ Assets/_Scripts/EnvironmentInteractions.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class EnvironmentInteractions : MonoBehaviour
+{
+    [SerializeField] private float range = 50f;
+
+    private FloorButton previousTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousTarget = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonInteraction();
+    }
+
+    void ButtonInteraction()
+    {
+        Vector3 rayOrigin = transform.position + 1 * Vector3.up;
+        Vector3 rayDirection = -transform.up;
+
+        Debug.DrawRay(rayOrigin, rayDirection * range, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, rayDirection, out hit, range))
+        {
+            FloorButton button = hit.collider.gameObject.GetComponent<FloorButton>();
+
+            if (previousTarget != button && previousTarget != null) previousTarget.Activate(false);
+
+            previousTarget = button;
+
+            if (button != null)
+            {
+                button.Activate(true);
+            }
+        }
+        else if (previousTarget != null) previousTarget.Activate(false);
+    }
+}
---
+++ Assets/_Scripts/FloorButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FloorButton : MonoBehaviour
+{
+    [SerializeField] private Material activeMaterial;
+    [SerializeField] private Material inactiveMaterial;
+
+    private Renderer _renderer;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Activate(bool activate)
+    {
+        if (activate) _renderer.material = activeMaterial;
+        else _renderer.material = inactiveMaterial;
+    }    
+}
