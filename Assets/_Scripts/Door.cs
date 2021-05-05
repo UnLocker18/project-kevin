63e36473850c56c84f137307a52db9ff0430085e
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-05-05 23:03:55 +0200

    Added basic door

---
+++ Assets/_Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        transform.DORotate(new Vector3(0, 120, 0), 1);
+    }
+
+    public void Close()
+    {
+        transform.DORotate(new Vector3(0, 0, 0), 1);
+    }
+}
