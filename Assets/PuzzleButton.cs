4adb3a895b312d3d7e8c9409d3d3a693af699eb6
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-05-07 20:34:15 +0200

    Added basic puzzle button

---
+++ Assets/PuzzleButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PuzzleButton : MonoBehaviour
+{
+    [SerializeField] private Material activeMaterial;
+    [SerializeField] private Material inactiveMaterial;
+
+    private bool isActive = false;
+    private Renderer[] _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = gameObject.GetComponentsInChildren<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Toggle()
+    {
+        if (isActive)
+        {
+            Debug.Log("toggle deactivate");
+            _renderer[0].material = inactiveMaterial;
+        }
+        else
+        {
+            Debug.Log("toggle activate");
+            _renderer[0].material = activeMaterial;
+        }
+
+        isActive = !isActive;
+    }
+}
