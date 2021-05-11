79a38d7e38c9a0da67eb41b98bea0a6953cfb2a7
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-05-11 19:15:27 +0200

    Fixed door angle bug

--- Assets/_Scripts/EnvironmentInteractions.cs
+++ Assets/_Scripts/EnvironmentInteractions.cs
@@ -32,53 +32,53 @@ public class EnvironmentInteractions : MonoBehaviour
         }
     }
 
-    public void Interaction()
-    {
-        Vector3 rayOrigin = transform.position + 0.25f * Vector3.up;
-        Vector3 rayDirection = transform.forward;
+    //public void Interaction()
+    //{
+    //    Vector3 rayOrigin = transform.position + 0.25f * Vector3.up;
+    //    Vector3 rayDirection = transform.forward;
 
-        Debug.DrawRay(rayOrigin, rayDirection * range, Color.blue);
+    //    Debug.DrawRay(rayOrigin, rayDirection * range, Color.blue);
 
-        RaycastHit hit;
+    //    RaycastHit hit;
 
-        if (grabbing)
-        {
-            SmallBox smallBox = transform.GetComponentInChildren<SmallBox>();
+    //    if (grabbing)
+    //    {
+    //        SmallBox smallBox = transform.GetComponentInChildren<SmallBox>();
 
-            if(smallBox != null)
-            {
-                grabbing = !grabbing;
-                smallBox.Grab(gameObject, grabbing, transform.Find("GrabbingPoint").transform.position);
-            }
-        }
-        else
-        {
-            if (Physics.Raycast(rayOrigin, rayDirection, out hit, range))
-            {
-                BoxInteraction(hit);
-                PuzzleButtonInteraction(hit);
-            }
-        }        
-    }
+    //        if(smallBox != null)
+    //        {
+    //            grabbing = !grabbing;
+    //            smallBox.Grab(gameObject, grabbing, transform.Find("GrabbingPoint").transform.position);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (Physics.Raycast(rayOrigin, rayDirection, out hit, range))
+    //        {
+    //            BoxInteraction(hit);
+    //            PuzzleButtonInteraction(hit);
+    //        }
+    //    }        
+    //}
 
-    void BoxInteraction(RaycastHit hit)
-    {
-        SmallBox box = hit.collider.gameObject.GetComponent<SmallBox>();
+    //void BoxInteraction(RaycastHit hit)
+    //{
+    //    SmallBox box = hit.collider.gameObject.GetComponent<SmallBox>();
 
-        if (box != null)
-        {
-            grabbing = !grabbing;
-            box.Grab(gameObject, grabbing, transform.Find("GrabbingPoint").transform.position);
-        }
-    }
+    //    if (box != null)
+    //    {
+    //        grabbing = !grabbing;
+    //        box.Grab(gameObject, grabbing, transform.Find("GrabbingPoint").transform.position);
+    //    }
+    //}
 
-    void PuzzleButtonInteraction(RaycastHit hit)
-    {
-        PuzzleButton puzzleButton = hit.collider.gameObject.GetComponentInParent<PuzzleButton>();
+    //void PuzzleButtonInteraction(RaycastHit hit)
+    //{
+    //    PuzzleButton puzzleButton = hit.collider.gameObject.GetComponentInParent<PuzzleButton>();
 
-        if (puzzleButton != null)
-        {
-            puzzleButton.Toggle();
-        }
-    }
+    //    if (puzzleButton != null)
+    //    {
+    //        puzzleButton.Toggle();
+    //    }
+    //}
 }
