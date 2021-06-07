790812c08499a5d2b6a12eb3c919089f2ed3f21d
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-06-07 19:40:16 +0200

    Added camera switcher, added cameras in tutorial scene

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera from;
+    [SerializeField] private CinemachineVirtualCamera to;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            from.Priority = 0;
+            to.Priority = 1;
+        }
+    }
+}
