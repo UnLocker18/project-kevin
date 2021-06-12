3659c496a79bd830512eeb4f36bfc2b8108fd911
DESKTOP-QIABJJB\giang <twh1897@gmail.com>
2021-06-13 00:01:43 +0200

    Improved input system, minor updates

--- Assets/_Scripts/AdditionalControls.cs
+++ Assets/_Scripts/AdditionalControls.cs
@@ -4,31 +4,30 @@ public class AdditionalControls : MonoBehaviour
 {
     private EnvironmentInteractions environmentInteractions;
     private DialogueTrigger dialogueTrigger;
+    private DialogueManager dialogueManager;
+    private CustomPauseMenu customPauseMenu;
 
     // Start is called before the first frame update
     void Start()
     {
         environmentInteractions = GetComponent<EnvironmentInteractions>();
         dialogueTrigger = GetComponent<DialogueTrigger>();
+        dialogueManager = FindObjectOfType<DialogueManager>();
+        customPauseMenu = FindObjectOfType<CustomPauseMenu>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            environmentInteractions.Interaction();
-        }
+        if (Input.GetButtonDown("Interact")) environmentInteractions.Interaction();
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            environmentInteractions.StickRope();
-        }
+        if (Input.GetButtonDown("Interact2")) environmentInteractions.StickRope();
+        
+        if (Input.GetButtonDown("Interact3")) environmentInteractions.LeaveRope();        
 
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            environmentInteractions.LeaveRope();
-        }
+        if (Input.GetButtonDown("ContinueDialogue")) dialogueManager.DisplayNextSentence();
+
+        if (Input.GetButtonDown("Pause")) customPauseMenu.ToggleMenu();
 
         if (Input.GetKeyDown(KeyCode.Alpha1)) environmentInteractions.SetPersonality(0);
         if (Input.GetKeyDown(KeyCode.Alpha2)) environmentInteractions.SetPersonality(1);
