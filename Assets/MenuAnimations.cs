54e5723fe4550e2dcb57d047e682347d96e2e3b3
Gianluca Garganese <twh1897@gmail.com>
2021-06-25 00:30:37 +0200

    Added menu animations

---
+++ Assets/MenuAnimations.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuAnimations : MonoBehaviour
+{
+    private int randomPersonality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomPersonality = Random.Range(0, 2);
+        
+        transform.GetChild(randomPersonality).gameObject.SetActive(true);
+
+        if (randomPersonality == 2)
+        {
+            StartCoroutine("SpoAnimation");
+        }        
+    }
+
+    private IEnumerator SpoAnimation()
+    {
+        Animator animator = transform.GetChild(2).GetComponent<Animator>();
+        
+        int i = 0;
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            animator.SetInteger("SpoMove", i);
+
+            if (i == 15) i = 0;
+            i++;
+        }
+    }
+}
